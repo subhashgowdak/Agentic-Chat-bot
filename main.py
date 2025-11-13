5223550a05ff4e71060f218ffd1d8f821c1715a2
Subhash <subhashgowdak03@gmail.com>
2025-11-13 10:31:31 +0530

    Specifying task for model

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import PromptTemplate
 
 model=OllamaLLM(model="llama3.2")
+
+template = """
+You are a helpful assistant that helps people find information.
+here are some relevant data: {data}
+Please answer the following question: {question}
+If you don't know the answer, just say that you don't know, don't try to make up an answer."""
