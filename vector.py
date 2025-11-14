13040b7ae015e366bac0346deead9de54fbd970d
Subhash <subhashgowdak03@gmail.com>
2025-11-14 14:08:33 +0530

    added embedding model & read .csv file

--- vector.py
+++ vector.py
@@ -3,3 +3,6 @@ from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import os
 import pandas as pd
+
+df = pd.read_csv("people-100.csv")
+embeddings = OllamaEmbeddings(model="mxbai-embed-large")
